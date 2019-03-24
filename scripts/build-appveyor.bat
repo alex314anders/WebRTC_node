b1be8f6c1c2f9b01bb85c9c4f30c8f8cacee8e4a
alex314anders <alex314anders@gmail.com>
2019-03-24 22:56:04 +1000

    first commit

---
+++ scripts/build-appveyor.bat
@@ -0,0 +1,42 @@
+@ECHO OFF
+SETLOCAL
+SET EL=0
+
+IF /I "%platform%"=="x64" powershell Install-Product node $env:nodejs_version x64
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+ECHO npm install
+SET SKIP_DOWNLOAD=true
+CALL npm install
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+:CHECK_NPM_TEST_ERRORLEVEL
+ECHO npm run lint
+CALL npm run lint
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+ECHO npm test
+CALL npm test
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+ECHO npm run test:browsers
+CALL npm run test:browsers
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+ECHO node-pre-gyp package
+CALL node_modules\.bin\node-pre-gyp package
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+SET msvs_version=2017
+IF NOT "%APPVEYOR_REPO_COMMIT_MESSAGE%" == "%APPVEYOR_REPO_COMMIT_MESSAGE:[publish binary]=%" (ECHO node-pre-gyp publish && CALL node_modules\.bin\node-pre-gyp --msvs_version=%msvs_version% publish)
+IF %ERRORLEVEL% NEQ 0 GOTO ERROR
+
+GOTO DONE
+
+:ERROR
+ECHO ERRORLEVEL^: %ERRORLEVEL%
+SET EL=%ERRORLEVEL%
+
+:DONE
+
+EXIT /b %EL%
---
+++ test/all.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const semver = require('semver');
+
+require('./closing-data-channel');
+require('./closing-peer-connection');
+require('./connect');
+require('./create-offer');
+require('./get-configuration');
+require('./i420helpers');
+require('./iceservers');
+require('./mediastream');
+require('./pass-interface-to-method');
+require('./rtcaudiosink');
+require('./rtcaudiosource');
+require('./rtcdatachannel');
+require('./rtcrtpreceiver');
+require('./rtcvideosink');
+require('./send-arraybuffer');
+require('./sessiondesc');
+
+// TODO(mroberts): This, and other tests in this block, use syntax incompatible
+// with Node 6. Rewriting these tests to avoid async/await is not worth it, nor
+// is introducing Babel.
+//
+// Therefore, we skip them. Remove this if-statement once we drop support for
+// Node 6.
+if (semver(process.version).major > 6) {
+  require('./rtcvideosource');
+}
+
+// TODO(mroberts): These two tests use simple-peer, which introduced syntax
+// incompatible with Node 6 in
+//
+//   https://github.com/feross/simple-peer/pull/355 introduced
+//
+// Therefore, we skip them. Remove this if-statement once we drop support for
+// Node 6.
+if (semver(process.version).major > 6) {
+  require('./multiconnect');
+  require('./custom-settings');
+}
+
+// TODO(mroberts): async_hooks were introduced in Node 9. We use them to test
+// that destructors fire at the appropriate time (and hence, no memory leaks
+// occur). Once we drop support for Node < 9, remove this.
+if (semver(process.version).major >= 9 && typeof gc === 'function') {
+  require('./destructor');
+}
